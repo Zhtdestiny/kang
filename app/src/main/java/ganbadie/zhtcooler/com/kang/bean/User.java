090f2dc5a944885d97799ad00ca7e08e0651c820
zht <kingnity@163.com>
2016-10-26 16:34:06 +0800

    第一次提交3

---
+++ app/src/main/java/ganbadie/zhtcooler/com/kang/bean/User.java
@@ -0,0 +1,12 @@
+package ganbadie.zhtcooler.com.kang.bean;
+
+/**
+ * Created by tarena on 2016/10/26.
+ */
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
